fc3bfffcc46859ff1410869ffc42abd6550689ce
Quinn Michaels <quinn@indra.ai>
2018-11-15 09:29:10 -0800

    vuejs cleanup, stylus architecture, and templating work

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main id="app">
     <section class="container-col">
+      <caption>Regular Headings</caption>
       <h1 v-text="h1"></h1>
       <h2 v-text="h2"></h2>
       <h3 v-text="h3"></h3>
@@ -15,36 +16,108 @@
       <h5 class="display" v-text="h5"></h5>
       <h6 class="display" v-text="h6"></h6>
     </section>
+    <caption>Check out these form elements in development for your 👀.</caption>
     <section class="container-col">
-      <div><button>button</button></div>
-      <div><button class="sm">small button</button></div>
-      <div><button class="lg">large button</button></div>
-      <div><input type="text" name="text" placeholder="Input..."></div>
-      <div><textarea name="textarea" placeholder="Textarea..."></textarea></div>
+      <form>
+        <div><button>button</button></div>
+        <div><button class="sm">small button</button></div>
+        <div><button class="lg">large button</button></div>
+        <div><input type="text" name="text" placeholder="Input..."></div>
+        <div><textarea name="textarea" placeholder="Textarea..."></textarea></div>
+      </form>
+    </section>
+    <caption>These are the color classes</caption>
+    <section class="colors">
+      <div v-for="color in (colors)">
+        <div :class="colorText(color)"><strong>{{color}}</strong>The color text</div>
+        <div :class="colorBg(color)"><strong>{{color}}</strong>The color background</div>
+        <div :class="colorBorder(color)"><strong>{{color}}</strong>The color border</div>
+      </div>
+    </section>
+    <caption>These are the various icons that are included using the Linear Icons Free package.</caption>
+    <section class="icons">
+      <div>
+        <span v-for="icon in icons"><i :class="iconClass(icon)"></i><br>.icn-{{icon}}</span>
+      </div>
     </section>
   </main>
 </template>
 
 <script>
+  // in this part of the component we see basic data creation where we can look through colors and set the text of the heading tags from the data.
   export default {
     name: 'App',
     data() {
-      return {
-        h1: 'H1 Title Heading',
-        h2: 'H2 Title Heading',
-        h3: 'H3 Title Heading',
-        h4: 'H4 Title Heading',
-        h5: 'H5 Title Heading',
-        h6: 'H6 Title Heading',
+      return {};
+    },
+    computed: {
+      h1() {
+        return this.$store.getters['global/h1'];
+      },
+      h2() {
+        return this.$store.getters['global/h2'];
+      },
+      h3() {
+        return this.$store.getters['global/h3'];
+      },
+      h4() {
+        return this.$store.getters['global/h4'];
+      },
+      h5() {
+        return this.$store.getters['global/h5'];
+      },
+      h6() {
+        return this.$store.getters['global/h6'];
+      },
+      colors() {
+        return this.$store.getters['global/colors'];
+      },
+      icons() {
+        return this.$store.getters['global/icons'];
       }
     },
-    computed: {},
+    methods: {
+      colorText(color) {
+        return `${color}-txt`;
+      },
+      colorBg(color) {
+        return `${color}-bg`;
+      },
+      colorBorder(color) {
+        return `${color}-border`;
+      },
+      iconClass(icon) {
+        return `icn-${icon}`
+      }
+    },
     components: {}
   }
 </script>
 
 <style lang="stylus">
+// here we import all the base styles into the template for use
+// then below that we can overwrite or modify or create any necessary styles for the current template.
   @require '../styles'
 
+  .colors
+    margin: 1rem
+    > div
+      > div
+        margin: .3rem
+        font-size: 1.5rem
+        padding: 1rem
 
+  .icons
+    display: flex
+    flex-flow: row wrap
+    align-items: center
+    align-content: center
+    justify-content: center
+    text-align: center
+    span
+      flex: 1 auto
+      padding: 1rem
+      display: inline-block
+      i
+        font-size: 2rem
 </style>
